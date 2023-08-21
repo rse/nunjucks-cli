27e65d636cc1f27755e0c61eb3e25dd2a7994e4a
Ralf S. Engelschall <rse@engelschall.com>
2023-08-21 08:56:16 +0200

    switch option parser from yargs to commander in order to support -Dx=y and not just -D x=y (with space)

--- nunjucks.js
+++ nunjucks.js
@@ -6,48 +6,41 @@
 */
 
 /*  own information  */
-const my       = require("./package.json")
+const my        = require("./package.json")
 
 /*  internal requirements  */
-const fs       = require("node:fs")
-const path     = require("node:path")
+const fs        = require("node:fs")
+const path      = require("node:path")
 
 /*  external requirements  */
-const yargs    = require("yargs")
-const chalk    = require("chalk")
-const jsYAML   = require("js-yaml")
-const nunjucks = require("nunjucks")
+const commander = require("commander")
+const chalk     = require("chalk")
+const jsYAML    = require("js-yaml")
+const nunjucks  = require("nunjucks")
 
 /*  parse command-line arguments  */
-const argv = yargs
-    .usage("Usage: nunjucks " +
-        "[-h|--help] " +
-        "[-V|--version] " +
-        "[-c|--config <config-file>] " +
-        "[-C|--option <key>=<value>] " +
-        "[-d|--defines <context-file>] " +
-        "[-D|--define <key>=<value>] " +
-        "[-e|--extension <module-name>] " +
-        "[-o|--output <output-file>|-] " +
-        "<input-file>|-")
-    .version(false)
-    .help("h").alias("h", "help").default("h", false).describe("h", "show usage help")
-    .boolean("V").alias("V", "version").describe("V", "show program version information")
-    .string("c").nargs("c", 1).alias("c", "config").describe("c", "load Nunjucks configuration YAML file")
-    .array("C").nargs("C", 1).alias("C", "option").describe("C", "set Nunjucks configuration option")
-    .string("d").nargs("d", 1).alias("d", "defines").describe("d", "load context definition YAML file")
-    .array("D").nargs("D", 1).alias("D", "define").describe("D", "set context definition key/value")
-    .array("e").nargs("e", 1).alias("e", "extension").describe("e", "load Nunjucks JavaScript extension module")
-    .string("o").nargs("o", 1).alias("o", "output").default("o", "-").describe("o", "save output file")
-    .strict()
-    .showHelpOnFail(true)
-    .demand(0)
-    .parserConfiguration({
-        "short-option-groups": true,
-        "parse-numbers":       true,
-        "boolean-negation":    true
-    })
-    .parse(process.argv.slice(2))
+const program = new commander.Command()
+program.name("nunjucks")
+    .description("Nunjucks Template Rendering Command-Line Interface")
+    .showHelpAfterError("hint: use option --help for usage information")
+    .option("-h, --help", "show usage help", false)
+    .option("-V, --version", "show program version information", false)
+    .option("-c, --config <config-file>", "load Nunjucks configuration YAML file", "")
+    .option("-C, --option <key>=<value>", "set Nunjucks configuration option", (v, l) => l.concat([ v ]), [])
+    .option("-d, --defines <context-file>", "load context definition YAML file", "")
+    .option("-D, --define <key>=<value>", "set context definition key/value", (v, l) => l.concat([ v ]), [])
+    .option("-e, --extension <module-name>", "load Nunjucks JavaScript extension module", (v, l) => l.concat([ v ]), [])
+    .option("-o, --output <output-file>", "save output file", "-")
+    .argument("[<input-file>]", "input file")
+program.parse(process.argv)
+const argv = { ...program.opts(), _: program.args }
+
+/*  handle special help request  */
+if (argv.help) {
+    console.log(program.helpInformation())
+    console.log("Example:\n  $ echo \"Hello, {{ who }}!\" | nunjucks -Dwho=World -\n")
+    process.exit(0)
+}
 
 /*  handle special version request  */
 if (argv.version) {
@@ -60,11 +53,11 @@ if (argv.version) {
 
 /*  read input file  */
 let input = ""
-if (argv._.length !== 1) {
-    console.error(chalk.red("nunjucks: ERROR: missing input file"))
+if (argv._.length > 1) {
+    console.error(chalk.red("nunjucks: ERROR: invalid number of arguments (zero or one input file expected)"))
     process.exit(1)
 }
-let inputFile = argv._[0]
+let inputFile = argv._[0] ?? "-"
 if (inputFile === "-") {
     inputFile = "<stdin>"
     process.stdin.setEncoding("utf-8")
@@ -109,14 +102,12 @@ if (argv.defines) {
 context.env = process.env
 
 /*  add context defines  */
-if (argv.define) {
-    argv.define.forEach((define) => {
-        let [ , key, val ] = define.match(/^([^=]+)(?:=(.*))?$/)
-        if (val === undefined)
-            val = true
-        context[key] = val
-    })
-}
+argv.define.forEach((define) => {
+    let [ , key, val ] = define.match(/^([^=]+)(?:=(.*))?$/)
+    if (val === undefined)
+        val = true
+    context[key] = val
+})
 
 /*  determine Nunjucks options  */
 let options = {}
@@ -129,10 +120,8 @@ if (argv.config) {
         process.exit(1)
     }
 }
-if (argv.options) {
-    console.log(argv.options)
-    options = Object.assign(options, argv.options)
-}
+if (argv.option.length > 0)
+    options = Object.assign(options, argv.option)
 options = Object.assign({}, {
     autoescape:       false,
     throwOnUndefined: false,
@@ -146,28 +135,26 @@ options = Object.assign({}, {
 const env = nunjucks.configure(inputFile, options)
 
 /*  load external extension files  */
-if (typeof argv.extension === "object" && argv.extension instanceof Array) {
-    for (let extension of argv.extension) {
-        let modpath = path.resolve(extension)
-        if (!fs.existsSync(modpath)) {
-            try {
-                modpath = require.resolve(extension)
-            }
-            catch (ex) {
-                modpath = null
-            }
+for (const extension of argv.extension) {
+    let modpath = path.resolve(extension)
+    if (!fs.existsSync(modpath)) {
+        try {
+            modpath = require.resolve(extension)
         }
-        if (modpath === null) {
-            console.error(chalk.red(`nunjucks: ERROR: failed to find extension module: ${extension}`))
-            process.exit(1)
+        catch (ex) {
+            modpath = null
         }
-        const mod = require(modpath)
-        if (!(mod !== null && typeof mod === "function")) {
-            console.error(chalk.red(`nunjucks: ERROR: failed to call extension file: ${modpath}`))
-            process.exit(1)
-        }
-        mod(env)
     }
+    if (modpath === null) {
+        console.error(chalk.red(`nunjucks: ERROR: failed to find extension module: ${extension}`))
+        process.exit(1)
+    }
+    const mod = require(modpath)
+    if (!(mod !== null && typeof mod === "function")) {
+        console.error(chalk.red(`nunjucks: ERROR: failed to call extension file: ${modpath}`))
+        process.exit(1)
+    }
+    mod(env)
 }
 
 /*  render Nunjucks template  */
